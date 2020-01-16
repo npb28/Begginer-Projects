b4e53d2f4565a6e9fa12548696e4073e68696253
Pierre <54605473+npb28@users.noreply.github.com>
2020-01-16 15:34:59 -0600

    Update 99_Bottles.cpp

--- 99_Bottles.cpp
+++ 99_Bottles.cpp
@@ -9,9 +9,6 @@ class Song {
 
   public:
     Song(); //default constuctor
-    int getBottles(); //return num of bottles
-    void decreaseBottle(); //class method to remove bottle
-    void printLyric(); //class method to print lyrics
     void validateInput(int); //validated user's input
     void startingBottles(); //ask user for num of bottles
     void printSong(); //print song with correct num of bottles
@@ -30,19 +27,9 @@ Song::Song() //default constructor
   startingBottles(); //calling method to get num of bottles
 }
 
-int Song::getBottles() //return num of bottles
-{
-  return bottles;
-}
-
-void Song::decreaseBottle() //decrement num of bottles
-{
-  bottles--;
-}
-
 void Song::validateInput(int input) //validate input
 {
-  if(input > 0)
+  if(input > 0 && input < 100)
   {
     validInput = true;
   }
@@ -54,37 +41,28 @@ void Song::validateInput(int input) //validate input
 
 void Song::startingBottles() //get user's starting bottle num
 {
-  int startingBottles;
-
   do
   {
     cout << "How many bottles would you like to start the song with ?" << endl;
-    cin >> startingBottles;
+    cin >> bottles;
 
-    validateInput(startingBottles);
+    validateInput(bottles);
 
   }while(validInput == false);
 
-  bottles = startingBottles;
-
 }
 
-void Song::printSong() //print entire song's lyrics
+void Song::printSong() //print song's lyrics
 {
-  for(int num = 0; num < bottles; num++) // call class methods
+  int startingBottles = bottles;
+  int oneLessBottle = bottles - 1;
+
+  for(int num = 0; num < startingBottles; num++) // call class methods
   {
-    printLyric();
-    decreaseBottle();
+    cout << bottles << " bottles of beer on the wall, " << bottles--;
+    cout << " bottles of beer. Take one down and pass it around, ";
+    cout << oneLessBottle-- << " bottles of beer on the wall" << endl;
+    cout << " " << endl;
   }
 }
 
-void Song::printLyric() //print one lyrical verse
-{
-
-  int bottlesLeft = bottles - 1;
-
-  cout << bottles << " bottles of beer on the wall, " << bottles;
-  cout << " bottles of beer. Take one down and pass it around, ";
-  cout << bottlesLeft << " bottles of beer on the wall" << endl;
-  cout << " " << endl;
-}
